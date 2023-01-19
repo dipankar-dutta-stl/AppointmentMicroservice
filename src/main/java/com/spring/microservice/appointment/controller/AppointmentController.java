a345cf9002bfe2d5e48b8c9e4f0f8c9f9bdb9a66
dipankar-dutta-stl <dipankar.dutta@stl.tech>
2023-01-20 00:06:28 +0530

    200120230006

--- src/main/java/com/spring/microservice/appointment/controller/AppointmentController.java
+++ src/main/java/com/spring/microservice/appointment/controller/AppointmentController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,7 @@ import com.spring.microservice.appointment.model.AppointmentDetails;
 import com.spring.microservice.appointment.repo.AppoinmentDetailsRepo;
 
 @RestController
+@CrossOrigin("http://localhost:4200/")
 @RequestMapping("api/v1/appointment/")
 public class AppointmentController {
 
@@ -49,6 +52,16 @@ public class AppointmentController {
 			return appoinmentDetailsRepo.findByDOCTOR_ID(did);
 		}catch(Exception x) {
 			return null;
+		}
+	}
+	
+	@PutMapping("update")
+	public String updateAppointment(@RequestBody AppointmentDetails ad) {
+		try {
+			appoinmentDetailsRepo.save(ad);
+			return "UPDATE SUCCESSFULLY";
+		}catch(Exception x) {
+			return "UPDATE FAILED";
 		}
 	}
 }
