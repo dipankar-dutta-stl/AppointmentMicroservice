0801a184371392ba083bf8b91f6e5fc12b3c5c10
dipankar-dutta-stl <dipankar.dutta@stl.tech>
2023-02-20 12:47:08 +0530

    200220231247

--- src/main/java/com/spring/microservice/appointment/controller/AppointmentController.java
+++ src/main/java/com/spring/microservice/appointment/controller/AppointmentController.java
@@ -10,14 +10,7 @@ import java.util.UUID;
 import com.spring.microservice.appointment.pdf.PdfGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import com.spring.microservice.appointment.model.AppointmentData;
@@ -105,6 +98,17 @@ public class AppointmentController {
             return "FOUND";
         } else {
             return "NOT FOUND";
+        }
+
+    }
+
+    @DeleteMapping("delete/{id}")
+    public String deleteAppointmentById(@PathVariable("id") String id){
+        try{
+          appoinmentDetailsRepo.deleteById(id);
+          return "APPOINTMENT DELETED";
+        }catch(Exception x){
+            return "ERROR";
         }
 
     }
