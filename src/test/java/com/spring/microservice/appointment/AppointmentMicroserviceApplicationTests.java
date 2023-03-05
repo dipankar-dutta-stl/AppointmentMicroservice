bc17b02c829a0fb70a597fc9219115316ef74e78
dipankar-dutta-stl <dipankar.dutta@stl.tech>
2023-03-05 23:44:28 +0530

    050320232344

--- src/test/java/com/spring/microservice/appointment/AppointmentMicroserviceApplicationTests.java
+++ src/test/java/com/spring/microservice/appointment/AppointmentMicroserviceApplicationTests.java
@@ -24,6 +24,27 @@ class AppointmentMicroserviceApplicationTests {
          System.out.println(data);
      }
 
+    @Test
+    void TestGetAppointmentByDoctorId() {
+        String data = given()
+                .header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON)
+                .when()
+                .get("http://localhost:8003/api/v1/appointment/get/d/83249056-b2f2-4e64-bf31-7210a7be8df1")
+                .then().assertThat().statusCode(200).extract().response().asString();
+        System.out.println(data);
+    }
+
+
+    @Test
+    void TestGetSlot() {
+        String data = given()
+                .header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON)
+                .when()
+                .get("http://localhost:8003/api/v1/appointment/slot/12bb9351-bdc1-494b-a864-fc26001b43ba")
+                .then().assertThat().statusCode(200).extract().response().asString();
+        System.out.println(data);
+    }
+
 
 
 
